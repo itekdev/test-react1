012e859ba9ddaeffbc029f141f1e853000a83b07
itekdev <42906332+itekdev@users.noreply.github.com>
2019-01-09 01:54:36 +0800

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Home = () => (
   <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
+    <h1>Welcome to the Tornadoes Website! HAHAHAH !!! </h1>
+    <h1> This is another line</h1>
   </div>
 )
 
